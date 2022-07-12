9092cf665fb7cc7bd3ffb25dc7d03a8f1e2d5f21
Elrendar <guuto9@gmail.com>
2022-07-12 09:35:39 +0900

    네이밍 룰 파일 추가

---
+++ naming_rules.py
@@ -0,0 +1,36 @@
+## 회원가입시 db에 저장할 데이터들 ##
+
+# name_receive = request.form['name_give']
+# email_receive = request.form['email_give']
+# password_receive = request.form['password_give']
+# birth_receive = request.form['birth_give']
+# campus_receive = request.form['campus_give']
+
+# doc = {
+#     'name':name_receive,
+#     'email':email_receive,
+#     'password':password_receive,
+#     'birth':birth_receive,
+#     #19960331
+#     'campus':campus_receive
+# }
+# db.campuspot.insert_one(doc)
+
+
+## 글 작성시 저장할 데이터들
+# write = {
+#     'title':title_receive,
+#     'email': email_receive,  #author
+#     'body':body_receive,
+#     'picture':picture_receive, # URL
+#     'tag':tag_receive,
+#     'campus':campus_receive
+# }
+
+
+## 미리 생성해둘 대학 리스트
+# 대학리스트 = {
+#     중앙대학교,
+#     숭실대학교,
+#     총신대학교
+# }
